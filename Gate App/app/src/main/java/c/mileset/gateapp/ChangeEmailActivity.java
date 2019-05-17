ba38bade9d6d49ec89ebdfcf0e5db8ef8350e94a
Dell <Prasad@9730>
2019-05-17 19:22:38 +0530

    1st Version

---
+++ Gate App/app/src/main/java/c/mileset/gateapp/ChangeEmailActivity.java
@@ -0,0 +1,13 @@
+package c.mileset.gateapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ChangeEmailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_change_email);
+    }
+}
